50fc9d74e62ce1256d1fcb4e81e27d1325f3ded8
Rakesh Chetla <rakesh.chetla@blenheimchalcot.com>
2023-02-02 12:26:29 +0530

    Update 2 in test.java

--- test.java
+++ test.java
@@ -1,3 +1,11 @@
+<<<<<<< HEAD
 Testing from ct
 
 Please do not delete this line
+=======
+class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, BC India!"); 
+    }
+}
+>>>>>>> 7554c45 (Update in test.java)
